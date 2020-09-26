88b6bfa8fc27789f42fe2f7714177761c5c8ab7f
Devan <devankav7@gmail.com>
2020-09-26 15:10:34 -0400

    moved food to data.dart

--- clay_hack/lib/main.dart
+++ clay_hack/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import "data.dart";
 
 void main() {
   debugPaintSizeEnabled = false;
@@ -131,15 +132,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class Product {
-  final String name;
-  final double price;
-  final String image;
-  var count = 0;
-
-  Product({this.name, this.price, this.image});
-}
-
 class GridPage extends StatefulWidget {
   const GridPage({Key key}) : super(key: key);
 
@@ -148,109 +140,7 @@ class GridPage extends StatefulWidget {
 }
 
 class GridPageState extends State<GridPage> {
-  var food = [
-    Product(
-        name: "Arizona",
-        price: 1.00,
-        image:
-            "https://www.riteaid.com/shop/media/catalog/product/6/1/613008715267.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=406&width=406&canvas=406:406"),
-    Product(
-        name: "Coke",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.j8VMQERIDHxKwwAl2iG6OQD6D6?w=148&h=180&c=7&o=5&dpr=1.25&pid=1.7    "),
-    Product(
-        name: "Sprite",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.Ud1jpVK6c7KXKavFP9QAsgAAAA?w=137&h=150&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Fanta",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.tQioW09wynomo1irQwOBVgHaHa?w=193&h=193&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Milk",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.PILBMXnk6Dd0Ohm11Cp2mwHaHa?w=197&h=197&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Juice",
-        price: 1.00,
-        image:
-            "https://i5.walmartimages.com/asr/4436eb83-e8e7-40d9-a80f-67f0a8c96364_1.6c2ce7267ef33c75bed2e674c19d6aa4.jpeg"),
-    Product(
-        name: "Bread",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th?q=Sliced+Bread&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.25&pid=InlineBlock&mkt=en-US&adlt=moderate&t=1&mw=247"),
-    Product(
-        name: "Bagel",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.SzWppaNNEKA5qinEBX-OGwHaFy?w=237&h=186&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Wrap",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.e6Ixp6e7PeYzyzOp1DCP7QHaEK?w=318&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Fresh Produce",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.m1TDL3O46CdClIQHtI9ztgHaIG?w=147&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Breakfast Sandwich",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.W4dmUSEY5Vod_FjJ8ha_GAHaDt?w=203&h=101&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Deli Meat",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.2HmLPCd_6NaPzKJIJhBLiAHaHa?w=166&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Cereal",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.9PHkXmBr_RZcdAZcwYCe5QHaHa?w=193&h=193&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Soup",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.2_Zx6-mNSnw4W0aKpHRkGAHaEK?w=276&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Candy",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.asXyUcJKgdRxmFrLmE7RjQHaHa?w=209&h=209&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Chips",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.YQbSmyEQph4Y_hP01OEzcgHaE8?w=181&h=141&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Pretzels",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OP.8bGhzagT4fjPDA474C474?w=160&h=150&rs=1&o=5&dpr=1.25&pid=21.1"),
-    Product(
-        name: "Cookies",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.Xe47pE1VHcwekD809pEZ6wHaE8?w=261&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Donuts",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.osmPNdu3wLCnIys8dQxMKgHaE8?w=276&h=184&c=7&o=5&dpr=1.25&pid=1.7"),
-    Product(
-        name: "Hot Pockets",
-        price: 1.00,
-        image:
-            "https://th.bing.com/th/id/OIP.43OWDYZNY9muqlyi8o0pjAHaHa?w=155&h=180&c=7&o=5&dpr=1.25&pid=1.7")
-  ];
-
+  
   void updateCount(index, add) {
     setState(() {
       if (add) {
