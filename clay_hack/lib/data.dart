00f2b1400204e528b2ca1d4925d111ab3abc329c
Devan <devankav7@gmail.com>
2020-10-03 21:07:26 -0400

    updated products

--- clay_hack/lib/data.dart
+++ clay_hack/lib/data.dart
@@ -9,103 +9,32 @@ class Product {
 
 var food = [
   Product(
-      name: "Arizona",
-      price: 1.79,
-      image:
-          "https://www.riteaid.com/shop/media/catalog/product/6/1/613008715267.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=406&width=406&canvas=406:406"),
-  Product(
-      name: "Coke",
-      price: 2.50,
-      image:
-          "https://th.bing.com/th/id/OIP.j8VMQERIDHxKwwAl2iG6OQD6D6?w=148&h=180&c=7&o=5&dpr=1.25&pid=1.7    "),
-  Product(
-      name: "Sprite",
-      price: 2.50,
-      image:
-          "https://th.bing.com/th/id/OIP.Ud1jpVK6c7KXKavFP9QAsgAAAA?w=137&h=150&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Fanta",
-      price: 2.50,
-      image:
-          "https://th.bing.com/th/id/OIP.tQioW09wynomo1irQwOBVgHaHa?w=193&h=193&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Milk",
-      price: 3.75,
-      image:
-          "https://th.bing.com/th/id/OIP.PILBMXnk6Dd0Ohm11Cp2mwHaHa?w=197&h=197&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Juice",
-      price: 2.10,
-      image:
-          "https://i5.walmartimages.com/asr/4436eb83-e8e7-40d9-a80f-67f0a8c96364_1.6c2ce7267ef33c75bed2e674c19d6aa4.jpeg"),
-  Product(
-      name: "Bread",
-      price: 3.25,
-      image:
-          "https://th.bing.com/th?q=Sliced+Bread&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.25&pid=InlineBlock&mkt=en-US&adlt=moderate&t=1&mw=247"),
-  Product(
-      name: "Bagel",
-      price: 2.75,
-      image:
-          "https://th.bing.com/th/id/OIP.SzWppaNNEKA5qinEBX-OGwHaFy?w=237&h=186&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Wrap",
-      price: 4.25,
-      image:
-          "https://th.bing.com/th/id/OIP.e6Ixp6e7PeYzyzOp1DCP7QHaEK?w=318&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Fresh Produce",
-      price: 2.20,
-      image:
-          "https://th.bing.com/th/id/OIP.m1TDL3O46CdClIQHtI9ztgHaIG?w=147&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Breakfast Sandwich",
-      price: 3.50,
-      image:
-          "https://th.bing.com/th/id/OIP.W4dmUSEY5Vod_FjJ8ha_GAHaDt?w=203&h=101&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Deli Meat",
-      price: 2.75,
-      image:
-          "https://th.bing.com/th/id/OIP.2HmLPCd_6NaPzKJIJhBLiAHaHa?w=166&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Cereal",
-      price: 1.75,
-      image:
-          "https://th.bing.com/th/id/OIP.9PHkXmBr_RZcdAZcwYCe5QHaHa?w=193&h=193&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Soup",
-      price: 2.50,
-      image:
-          "https://th.bing.com/th/id/OIP.2_Zx6-mNSnw4W0aKpHRkGAHaEK?w=276&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Candy",
-      price: 1.75,
-      image:
-          "https://th.bing.com/th/id/OIP.asXyUcJKgdRxmFrLmE7RjQHaHa?w=209&h=209&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Chips",
-      price: 1.75,
-      image:
-          "https://th.bing.com/th/id/OIP.YQbSmyEQph4Y_hP01OEzcgHaE8?w=181&h=141&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Pretzels",
-      price: 1.75,
-      image:
-          "https://th.bing.com/th/id/OP.8bGhzagT4fjPDA474C474?w=160&h=150&rs=1&o=5&dpr=1.25&pid=21.1"),
-  Product(
-      name: "Cookies",
-      price: 1.25,
-      image:
-          "https://th.bing.com/th/id/OIP.Xe47pE1VHcwekD809pEZ6wHaE8?w=261&h=180&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Donuts",
-      price: 2.00,
-      image:
-          "https://th.bing.com/th/id/OIP.osmPNdu3wLCnIys8dQxMKgHaE8?w=276&h=184&c=7&o=5&dpr=1.25&pid=1.7"),
-  Product(
-      name: "Hot Pockets",
-      price: 3.25,
-      image:
-          "https://th.bing.com/th/id/OIP.43OWDYZNY9muqlyi8o0pjAHaHa?w=155&h=180&c=7&o=5&dpr=1.25&pid=1.7")
+    name: "Sprite 12 Pack",
+    price: 7.29,
+    image: "https://s3-us-west-2.amazonaws.com/ldsbookstore/products/LDP-MTCS165-sprite.jpeg"
+  ), Product(
+    name: "Peace Tea",
+    price: 1.79,
+    image: "https://images.heb.com/is/image/HEBGrocery/002111738"
+  ), Product(
+    name: "Simple Raspberry Lemonade 52 oz.",
+    price: 4.19,
+    image: "https://sbfcdn.azureedge.net/media/0001845_simply-raspberry-lemonade-52-oz_600.jpeg"
+  ), Product(
+    name: "Chips Ahoy Original",
+    price: 4.89,
+    image: "https://images-na.ssl-images-amazon.com/images/I/81ak1LBmQsL._SY355_.jpg"
+  ), Product(
+    name: "Cosmic Brownies",
+    price: 1.99,
+    image: "https://images.heb.com/is/image/HEBGrocery/000390305"
+  ), Product(
+    name: "Pirouline Creme Filled Wafers",
+    price: 5.79,
+    image: "https://images-na.ssl-images-amazon.com/images/I/718bsM7jbeL._SX342_.jpg"
+  ), Product(
+    name: "Nissin Spicy Chicken Chow Mein",
+    price: 1.29,
+    image: "https://d2lhip1ki8p4nk.cloudfront.net/uploads/products/chow-mein/flavors/Chow_Mein_Spicy_Chicken_Front_328x287.png?mtime=20191210211801"
+  )
 ];
